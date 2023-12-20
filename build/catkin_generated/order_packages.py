63f5f971323a80507430309672726ecef1779b31
iris <laanearu.iris@gmail.com>
2023-12-20 11:48:49 +0000

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/root/RT1_Assignment_2/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/root/my_ros_workspace/devel;/opt/ros/noetic'.split(';') if '/root/my_ros_workspace/devel;/opt/ros/noetic' != '' else []
---
+++ devel/share/gennodejs/ros/assignment_2_2023/msg/_index.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+let PlanningFeedback = require('./PlanningFeedback.js');
+let PlanningResult = require('./PlanningResult.js');
+let PlanningActionGoal = require('./PlanningActionGoal.js');
+let PlanningGoal = require('./PlanningGoal.js');
+let PlanningActionResult = require('./PlanningActionResult.js');
+let PlanningActionFeedback = require('./PlanningActionFeedback.js');
+let PlanningAction = require('./PlanningAction.js');
+
+module.exports = {
+  PlanningFeedback: PlanningFeedback,
+  PlanningResult: PlanningResult,
+  PlanningActionGoal: PlanningActionGoal,
+  PlanningGoal: PlanningGoal,
+  PlanningActionResult: PlanningActionResult,
+  PlanningActionFeedback: PlanningActionFeedback,
+  PlanningAction: PlanningAction,
+};
